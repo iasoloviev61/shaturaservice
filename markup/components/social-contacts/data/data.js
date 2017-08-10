6ea7160c71c784461aff5e181cd9eb2ec5d5b1de
Igor Soloviev <iasoloviev61@gmail.com>
2017-08-10 15:51:29 +0300

    add new svg icons

---
+++ markup/components/social-contacts/data/data.js
@@ -0,0 +1,32 @@
+var data = {'socialContacts': {
+    vk: {
+        socialLink: "https://vk.com/id3040974",
+        socialIcon: '#vk',
+        socialName: 'vk'
+    },
+    in: {
+        socialLink: "https://ru.linkedin.com",
+        socialIcon: '#linkedin-logo',
+        socialName: 'linkedin-logo'
+    },
+    goggle: {
+        socialLink: "https://google.com",
+        socialIcon: '#google-logo',
+        socialName: 'google-logo'
+    },
+    email: {
+        socialLink: "https://mail.ru",
+        socialIcon: '#vk',
+        socialName: 'vk'
+    },
+    skype: {
+        socialLink: "https://vk.com/id3040974",
+        socialIcon: '#vk',
+        socialName: 'vk'
+    },
+    facebook: {
+        socialLink: "https://fb.com",
+        socialIcon: '#facebook-letter-logo',
+        socialName: 'facebook-letter-logo'
+    }
+}};
