fc1e56ba74ba2665d22f389097a9e333efcfa356
Igor Soloviev <iasoloviev61@gmail.com>
2017-08-11 14:34:49 +0300

    add signup block

--- markup/components/signup/data/data.js
+++ markup/components/signup/data/data.js
@@ -1,1 +1,17 @@
-var data = {signup: {}};
+var data = {signup: {
+    register: {
+        name: "Регистрация",
+        iconName: "register",
+        iconPath: "#add-people"
+    },
+    login: {
+        name: "Вход",
+        iconName: "login",
+        iconPath: "#login"
+    },
+    resetPassword: {
+        name: "Забыли пароль?",
+        iconName: "resetpassword",
+        iconPath: "#key-shape-interface-symbol-for-password"
+    }
+}};
