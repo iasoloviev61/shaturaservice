c1f6cefc6b70b07d0a53017ab7e1264d4be0d3d1
Igor Soloviev <iasoloviev61@gmail.com>
2017-08-11 11:10:01 +0300

    add header-top style and data

--- markup/components/social-contacts/data/data.js
+++ markup/components/social-contacts/data/data.js
@@ -2,31 +2,37 @@ var data = {'socialContacts': {
     vk: {
         socialLink: "https://vk.com/id3040974",
         socialIcon: '#vk',
-        socialName: 'vk'
+        socialName: 'vk',
+        socialText: 'Вконтакте'
     },
-    in: {
-        socialLink: "https://ru.linkedin.com",
-        socialIcon: '#linkedin-logo',
-        socialName: 'linkedin-logo'
-    },
-    goggle: {
-        socialLink: "https://google.com",
-        socialIcon: '#google-logo',
-        socialName: 'google-logo'
-    },
-    email: {
-        socialLink: "https://mail.ru",
-        socialIcon: '#vk',
-        socialName: 'vk'
-    },
-    skype: {
-        socialLink: "https://vk.com/id3040974",
-        socialIcon: '#vk',
-        socialName: 'vk'
-    },
+    // in: {
+    //     socialLink: "https://ru.linkedin.com",
+    //     socialIcon: '#linkedin-logo',
+    //     socialName: 'linkedin',
+    //     socialText: 'Вконтакте'
+    // },
+    // goggle: {
+    //     socialLink: "https://google.com",
+    //     socialIcon: '#google-logo',
+    //     socialName: 'google',
+    //     socialText: 'Вконтакте'
+    // },
+    // skype: {
+    //     socialLink: "https://vk.com/id3040974",
+    //     socialIcon: '#skype-logo',
+    //     socialName: 'skype',
+    //     socialText: 'Вконтакте'
+    // },
     facebook: {
         socialLink: "https://fb.com",
         socialIcon: '#facebook-letter-logo',
-        socialName: 'facebook-letter-logo'
+        socialName: 'facebook-letter-logo',
+        socialText: 'Вконтакте'
+    },
+    email: {
+        socialLink: "mailto:iasoloviev@gmail.com",
+        socialIcon: '#email-envelope-outline-shape-with-rounded-corners',
+        socialName: 'email',
+        socialText: 'iasoloviev@gmail.com'
     }
 }};
