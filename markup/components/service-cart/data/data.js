7bf48edfc50bea6a1b7a2ed546d0c857ba8e7e1e
Igor Soloviev <iasoloviev61@gmail.com>
2017-08-14 17:07:52 +0300

    add service cart block

---
+++ markup/components/service-cart/data/data.js
@@ -0,0 +1,37 @@
+var data = {'serviceCart': {
+    auto: {
+        serviceTitle: 'Автолюбителям',
+        serviceICon: '#car-side',
+        serviceIconName: 'car',
+        serviceLink: '/auto.html',
+        servicesList: ['Автотюнинг', 'Автоэлектрик', 'Автосервисы', 'Мойка автомобилей', 'Автострахование']
+    },
+    kids: {
+        serviceTitle: 'Для детей',
+        serviceICon: '#newborn',
+        serviceIconName: 'kids',
+        serviceLink: '/kids.html',
+        servicesList: ['Детский досуг', 'Мини сады', 'Развитие', 'Здоровье', 'Детские магазины', 'Игрушки']
+    },
+    food: {
+        serviceTitle: 'Кафе и рестораны',
+        serviceICon: '#restaurant',
+        serviceIconName: 'restaurant',
+        serviceLink: '/food.html',
+        servicesList: ['Кафе', 'Рестораны', 'Бары', 'Столовые']
+    },
+    deliver: {
+        serviceTitle: 'Доставка и курьеры',
+        serviceICon: '#dumper',
+        serviceIconName: 'dumper',
+        serviceLink: '/deliver.html',
+        servicesList: ['Такси', 'Грузоперевозки', 'Курьер', 'Прочее']
+    },
+    sport: {
+        serviceTitle: 'Спорт',
+        serviceICon: '#ball-of-basketball',
+        serviceIconName: 'ball',
+        serviceLink: '/sport.html',
+        servicesList: ['Фитнес клубы', 'Площадки', 'Мероприятия', 'Залы']
+    }
+}};
